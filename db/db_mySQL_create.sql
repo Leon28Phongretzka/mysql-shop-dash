266965c2d3bf99d6efaf718cb4049b962bfa4ba9
HiepSerina <tranthihongdiu1234@gmail.com>
2023-09-08 22:04:03 +0700

    admin

--- db/db_mySQL_create.sql
+++ db/db_mySQL_create.sql
@@ -481,7 +481,7 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `user_payment_method`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-/*tranhiep12345*/;
+/*tranhiepvip*/;
 CREATE TABLE `user_payment_method` (
   `id` int NOT NULL,
   `user_id` int NOT NULL,
